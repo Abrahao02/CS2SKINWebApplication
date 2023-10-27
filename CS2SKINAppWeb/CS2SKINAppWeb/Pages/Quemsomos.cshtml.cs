1d16e535fa64ab2fee4866d6be2b2e996d2c97fc
Abrahao <edsa.1909@gmail.com>
2023-10-27 01:20:34 -0300

    Initial commit

---
+++ CS2SKINAppWeb/CS2SKINAppWeb/Pages/Quemsomos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS2SKINAppWeb.Pages
+{
+    public class QuemsomosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
